995edb2b7eb3e9b26959d2c47fb8390004d4bc90
Eddy <eddy15201@gmail.com>
2023-01-29 11:17:31 +0800

    Refactoring types

--- src/types/game.ts
+++ src/types/game.ts
@@ -6,3 +6,34 @@ export interface Seen {
     opponent_name: string;
     card: SeenCard;
 }
+
+export interface NamedPlayer {
+    name: string;
+    score: number;
+}
+
+interface CardUsage {
+    can_discard: boolean;
+    can_guess_cards: Array<string>;
+    choose_players: Array<string>;
+}
+
+export interface HandCard {
+    name: string;
+    description: string;
+    usage: CardUsage;
+    value: number;
+}
+
+interface Players {
+    name: string;
+    out: boolean;
+    cards: Array<HandCard>;
+    seen_cards: Array<Seen>;
+}
+
+export interface Round {
+    players: Array<Players>;
+    turn_player: Players;
+    winner?: string;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,38 +1,7 @@
 import {GameEvent} from "@/types/event";
-import {Seen} from "@/types/game";
+import {NamedPlayer, Round} from "@/types/game";
 
 export type ViewState = "pick-name" | "game-list" | "game-room";
-
-export interface NamedPlayer {
-    name: string;
-    score: number;
-}
-
-interface CardUsage {
-    can_discard: boolean;
-    can_guess_cards: Array<string>;
-    choose_players: Array<string>;
-}
-
-export interface HandCard {
-    name: string;
-    description: string;
-    usage: CardUsage;
-    value: number;
-}
-
-interface Players {
-    name: string;
-    out: boolean;
-    cards: Array<HandCard>;
-    seen_cards: Array<Seen>;
-}
-
-interface Round {
-    players: Array<Players>;
-    turn_player: Players;
-    winner?: string;
-}
 
 export interface GameStatus {
     game_id: string;
@@ -40,3 +9,6 @@ export interface GameStatus {
     rounds: Array<Round>;
     events: Array<GameEvent>;
 }
+
+export * from "./event";
+export * from "./game";
